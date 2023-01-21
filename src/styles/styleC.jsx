47b9580caeb21d6927b4bb7254957cccf789b086
RadimYassin <radimyassin20@gamil.com>
2023-01-22 00:49:24 +0100

    update in styleC

---
+++ src/styles/styleC.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+export const Container=styled.section`
+    width:80%;
+    height:100vh;
+    margin: 0 auto;
+    border:1px solid blanchedalmond;
+
+`
+
+export const NavC=styled.div`
+    width:100%;
+    height:80px;
+    background-color: rgb(36, 113, 154);
+    display:flex;
+  
+    align-items:center;
+    justify-content:space-between;
+
+`
+
+
+export const Logo=styled.h3`
+  
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size:40px;
+    color:white;
+`
+
+export const Links=styled.div`
+   
+`
+export const LinkI=styled(Link)`
+   
+`
