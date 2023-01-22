4b7ee4ee6452401f49c6b6ed04ac53539ce44fc0
RadimYassin <radimyassin20@gamil.com>
2023-01-22 23:19:38 +0100

    some-changes ,create function onchnage

--- src/Component/Add.jsx
+++ src/Component/Add.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 import { BoxSearch,InputSearch,BoxAdd } from '../styles/styleC'
 
 const Add = () => {
+ const [serch,setSearch]=useState('')
+  const OnChange=e=>{
+    e.preventDefault()
+
+   setSearch(e.target.value)
+
+
+
+  console.log(e.target.value);
+
+  }
   return (
     <BoxAdd>
       <BoxSearch>
-      <InputSearch type='text'
-      placeholder='name of film or s'
+      <InputSearch 
+      value={serch}
+      onChange={OnChange}
+      type='text'
+      placeholder='search of a film'
       />
       </BoxSearch>
     </BoxAdd>
