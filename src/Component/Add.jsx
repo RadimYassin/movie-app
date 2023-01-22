d5f91316c00ea0d65741148427ee657ebf1c66ef
RadimYassin <radimyassin20@gamil.com>
2023-01-23 00:33:16 +0100

    some-changes ,usin api and fetching data

--- src/Component/Add.jsx
+++ src/Component/Add.jsx
@@ -5,16 +5,26 @@ import { BoxSearch,InputSearch,BoxAdd } from '../styles/styleC'
 
 const Add = () => {
  const [serch,setSearch]=useState('')
+ const [movies,setMovies]=useState([])
   const OnChange=e=>{
     e.preventDefault()
 
    setSearch(e.target.value)
 
     axios.get(`
-    https://api.themoviedb.org/3/search/movie?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1&include_adult=false&query=${e.target.value}`).then(res=>res).then(data=>console.log(data))
+    https://api.themoviedb.org/3/search/movie?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
+    .then(res=>res).then(data=>
+      {
+        if (!data.errors) {
+          setMovies(data.data.results)
+        }else{
+          setMovies([])
+        }
+      }
+      
+      )
 
-  console.log(import.meta.env.VITE_SOME_KEY);
-
+  console.log(movies);
   }
   return (
     <BoxAdd>
