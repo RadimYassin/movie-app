893ae6eb0e9af3264db2dec8480ee8800059ea6f
RadimYassin <radimyassin20@gamil.com>
2023-01-22 01:10:17 +0100

    update in styleC

--- src/styles/styleC.jsx
+++ src/styles/styleC.jsx
@@ -5,6 +5,10 @@ export const Container=styled.section`
     height:100vh;
     margin: 0 auto;
     border:1px solid blanchedalmond;
+    @media screen and (max-width:900px ) {
+        width:100%;
+  
+    }
 
 `
 
@@ -16,6 +20,7 @@ export const NavC=styled.div`
   
     align-items:center;
     justify-content:space-between;
+  
 
 `
 
@@ -24,12 +29,33 @@ export const Logo=styled.h3`
   
     font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     font-size:40px;
+    margin-left:10px;
     color:white;
+    flex:1;
+    @media screen and (max-width:900px ) {
+        font-size:30px;
+  
+    }
 `
 
 export const Links=styled.div`
-   
+
+   flex:1;
+   display:flex;
+   justify-content:center;
+   gap:10px;
+
 `
 export const LinkI=styled(Link)`
+   text-decoration:none;
+   color:white;
+   font-size:17px;
    
+    padding:8px;
+    border-radius:5px;
+
+   &:hover{
+    background-color: rgb(28, 132, 187);
+    transition: all s ease-in-out;
+   }
 `
