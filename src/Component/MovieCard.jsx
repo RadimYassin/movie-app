882dd64522b59831cae527285735861296875dc1
RadimYassin <radimyassin20@gamil.com>
2023-01-25 00:27:46 +0100

    some-changes

--- src/Component/MovieCard.jsx
+++ src/Component/MovieCard.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import { MovieBOX,Img,Info } from '../styles/styleC'
-
+import { MovieBOX,Img,Info,Title,Date,InfoC,Text,GroupeBtn, ButtonAdd } from '../styles/styleC'
+import {FcFilmReel} from 'react-icons/fc'
 const MovieCard = ({movie}) => {
   return (
         <MovieBOX>
-                 <Img></Img>
-                  <Info>g</Info>
+                 <Img >
+                
+                   <img style={{width:300,height:300}}  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+                
+                   </Img>
+                  <Info>
+                        <InfoC>
+                        <Title> <FcFilmReel/>{movie.title}</Title>
+                        <Date>{movie.release_date}</Date>
+                        </InfoC>
+                        <Text>
+                            <span style={{fontSize:20,marginBottom:10}}>Overview:</span>
+                        {movie.overview ? movie.overview :"reloading ..."}
+
+                        </Text>
+               
+                         <GroupeBtn>
+                             <ButtonAdd>
+                                add 
+                             </ButtonAdd>
+                             <ButtonAdd>
+                               delete
+                             </ButtonAdd>
+                             
+                         </GroupeBtn>
+           
+                  
+                  </Info>
 
         </MovieBOX>
   )
