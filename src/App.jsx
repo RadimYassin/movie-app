2f6fbcbe2ce68519626cb5150ed6a8988e343d4b
RadimYassin <radimyassin20@gamil.com>
2023-01-26 23:15:15 +0100

    update in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import Header from './Component/Header';
 import Add from './Component/Add';
-import  WatcheList from './Component/WatcheList';
+import WatcheList from './Component/WatcheList';
 import Watched from './Component/Watched';
 import { Container } from "./styles/styleC"
-import { Route ,Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { GProvider } from './context/Gstate';
 
 
 
 function App() {
 
   return (
- <Container>
- <Header/>
+    <GProvider>
+      <Container>
+        <Header />
 
-  <Routes>
-   <Route index path='/' element={<WatcheList/>}/>
-   <Route path='/Watched' element={<Watched/>}/>
-   <Route path='/Add' element={<Add/>}/>
+        <Routes>
 
-  </Routes>
- </Container>
+          <Route index path='/' element={<WatcheList />} />
+          <Route path='/Watched' element={<Watched />} />
+          <Route path='/Add' element={<Add />} />
+
+        </Routes>
+      </Container>
+    </GProvider>
   )
 }
 
