492a89f2171297714cb6db71fafc68f166fd476d
RadimYassin <radimyassin20@gamil.com>
2023-01-26 23:48:51 +0100

    some-changes

--- src/Component/WatcheList.jsx
+++ src/Component/WatcheList.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React ,{useContext} from 'react'
+import { Gcontext } from '../context/Gstate'
+import {BoxC,BoxM,Img,Info,InfoC,Text,Title,MovieBOX} from '../styles/styleC'
+import {FcFilmReel} from 'react-icons/fc'
 
 const WatcheList = () => {
+  const {watchlist}=useContext(Gcontext)
+  console.log(watchlist);
   return (
-    <div>
-      whatch ist
-    </div>
+    < BoxC>
+      <BoxM>
+      <h1> My warch list: </h1>
+      {watchlist.map(movie=> <MovieBOX>
+                 <Img >
+                
+                   <img style={{width:300,height:300}}  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+                
+                   </Img>
+                   
+                   <Info>
+                        <InfoC>
+                        <Title> <FcFilmReel/>{movie.title}</Title>
+                        <Date>{movie.release_date}</Date>
+                        </InfoC>
+                      
+                  
+                  </Info>
+                   
+                   
+                   </MovieBOX>
+        
+      )}
+      </BoxM>
+
+</  BoxC>
   )
 }
 
