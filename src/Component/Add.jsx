bbadc7c675ca3968c0e87fa8d14b56f782430740
RadimYassin <radimyassin20@gamil.com>
2023-02-05 23:39:18 +0100

    new update

--- src/Component/Add.jsx
+++ src/Component/Add.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useState } from 'react'
 import MovieCard from './MovieCard'
 import { BoxSearch,InputSearch,BoxAdd,BoxC,BoxM } from '../styles/styleC'
+import { client } from '../axios/Axios'
 
 const Add = () => {
  const [serch,setSearch]=useState('')
@@ -11,9 +12,7 @@ const Add = () => {
     e.preventDefault()
 
    setSearch(e.target.value)
-
-    axios.get(`
-    https://api.themoviedb.org/3/search/movie?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=100&include_adult=false&query=${e.target.value}`)
+     client.get(`search/movie?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=100&include_adult=false&query=${e.target.value}`)
     .then(res=>res).then(data=>
       {
         if (!data.errors) {
--- src/Component/TopRated.jsx
+++ src/Component/TopRated.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React from 'react'
 import { useEffect,useContext, useState} from 'react'
+import { client } from '../axios/Axios';
 import { Gcontext } from '../context/Gstate';
 
 import '../styles/style.css';
@@ -9,11 +10,9 @@ function TopRated() {
 const [topM,setTopM]=useState([])
 const {addwatchlist,watchlist} =useContext(Gcontext)
 
-  const url=`https://api.themoviedb.org/3/movie/top_rated?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1`
-
-
   useEffect(()=>{
-   axios.get(url).then(res=>setTopM(res.data.results))
+    client.get("/movie/top_rated?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1").then(res=>setTopM(res.data.results))
+  
   },[])
 
   return (
