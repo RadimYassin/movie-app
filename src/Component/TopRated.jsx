f861bb8e0ede95d15746ec010fde78be70c6eb11
RadimYassin <radimyassin20@gamil.com>
2023-02-05 23:16:34 +0100

    updating

--- src/Component/TopRated.jsx
+++ src/Component/TopRated.jsx
@@ -4,16 +4,18 @@ import { useEffect,useContext, useState} from 'react'
 import { Gcontext } from '../context/Gstate';
 
 import '../styles/style.css';
+import MovieTop from './MovieTop';
 function TopRated() {
 const [topM,setTopM]=useState([])
 const {addwatchlist,watchlist} =useContext(Gcontext)
+
   const url=`https://api.themoviedb.org/3/movie/top_rated?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1`
 
 
   useEffect(()=>{
    axios.get(url).then(res=>setTopM(res.data.results))
   },[])
-console.log(topM)
+
   return (
     <div className='movies'>
           
@@ -24,27 +26,7 @@ console.log(topM)
                   <ul className='list-movies'>
                          {
                           topM.map((movie,index)=>{
-                            return    <li key={index} className="movie" >
-                                        <h3>{index+1}</h3>
-                                        <div className="img">
-                                            <img style={{width:300,height:300}}  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
-                                        </div>
-                                        <div className='info'>
-                                                 <div className='info-general'>
-                                                        <h2>{movie.original_title}</h2>
-                                                       <span className='vote'>{movie.vote_average}</span> 
-                                                 </div>
-                                                 <div className='info-overview'>
-                                                     <p>{movie.overview}</p>
-                                                 </div>
-                                                 <div className='groupe-btn'>
-                                                    <button className='add'  onClick={()=>addwatchlist(movie)}>add to wahtchlist</button>
-                                                 </div>
-                                               
-                                        </div>
-                                        
-
-                                       </li>
+                            return   <MovieTop  key={index} movie={movie} index={index} watchlist={watchlist} addwatchlist={addwatchlist}/>
                           }
                             
                             
