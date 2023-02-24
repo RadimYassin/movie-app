4d27787f988bb4368431a934e7ac0a7df344534d
RadimYassin <radimyassin20@gamil.com>
2023-02-24 23:23:25 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ import { Container } from "./styles/styleC"
 import { Route, Routes } from 'react-router-dom';
 import { GProvider } from './context/Gstate';
 import TopRated from './Component/TopRated';
+import { useEffect } from 'react';
 
 
 
 function App() {
+
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:5000/").then(res=>res.json()).then(res=>console.log(res))
+  },[])
 
   return (
     <GProvider>
